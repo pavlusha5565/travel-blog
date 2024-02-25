4c6dbfd7228981bde1bda901dc8d024761902e5a
Pavel <pavlusha5565@gmail.com>
2024-02-25 13:34:41 +0300

    add docker pgdata to gitignore

---
+++ src/test/java/com/artifactIntelligence/travelbook/TravelbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artifactIntelligence.travelbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
